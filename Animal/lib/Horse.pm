bd3da3ebfd9b8a2132ce5dfedf4ee2e3fa18a691
Bill <bill@cloudigit.com>
2016-04-24 19:10:52 +0800

    Add the moudle we added from the last commit

---
+++ Animal/lib/Horse.pm
@@ -0,0 +1,9 @@
+package Horse;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
---
+++ Animal/lib/Sheep.pm
@@ -0,0 +1,9 @@
+package Sheep;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
