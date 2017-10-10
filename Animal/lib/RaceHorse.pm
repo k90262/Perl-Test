2fbe751d6628ff9a9d94a302d03e6a6986f34a88
Bill <piglikescomputer@yahoo.com.tw>
2017-10-10 17:20:27 +0800

    (Exercise 18-1) refactor as the answer

--- Animal/lib/RaceHorse.pm
+++ Animal/lib/RaceHorse.pm
@@ -2,29 +2,30 @@ package RaceHorse;
 use parent qw(Horse);
 
 dbmopen (our %STANDINGS, "standings", 0666)
-  or die "Cannot access standings dbm: $!";
+  or die "Cannot acess standings dbm: $!";
 
-# 擴充上層類別的建構函式
-sub named {
+sub named { # 類別方法
   my $self = shift->SUPER::named(@_);
   my $name = $self->name;
   my @standings = split ' ', $STANDINGS{$name} || "0 0 0 0";
-  @{$self}{qw(wins places shows losses)} = @standings; # use hash slice
+  @$self{qw(wins places shows losses)} = @standings;
   $self;
 }
 
+sub DESTROY { # 自動調用的實體方法
+  my $self = shift;
+  $STANDINGS{$self->name} = "@$self{qw(wins places shows losses)}";
+  $self->SUPER::DESTROY if $self->can( 'SUPER::DESTROY' );
+}
+
+## instance methods:
 sub won { shift->{wins}++; }
 sub placed { shift->{places}++; }
 sub showed { shift->{shows}++; }
 sub lost { shift->{losses}++; }
-sub standings {
-  my $self = shift;
-  join ', ', map "$self->{$_} $_", qw(wins places shows losses);
+sub standings { 
+  my $self = shift; 
+  join ", ", map "$self->{$_} $_", qw(wins places shows losses);
 }
 
-sub DESTROY {
-  my $self = shift;
-  $STANDINGS{$self->name} = "@{$self}{qw(wins places shows losses)}"; # use has slice
-  $self->SUPER::DESTROY if $self->can( 'SUPER::DESTROY' );
-}
 1;
